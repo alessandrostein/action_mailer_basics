7c56fb7ee5db7d88f931b338f32cfba92d4c2395
alessandrostein <alessandro@alessandrostein.com>
2014-10-31 15:27:36 -0200

    Templates

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,8 +1,6 @@
 class EmailMailer < ActionMailer::Base
   default from: "alessandro@alessandrostein.com"
 
-  layout 'application'
-
   def welcome_email(email)
     @email = email
     @url = 'www.alessandrostein.com'
@@ -15,6 +13,15 @@ class EmailMailer < ActionMailer::Base
     @email = email
     @url = 'www.alessandrostein.com'
     mail(to: @email.to, subject: @email.subject)
+  end
+
+  def welcome_template(email)
+    @email = email
+    @url = 'www.alessandrostein.com'
+    mail(to: @email.to,
+         subject: @email.subject,
+         template_path: 'layouts',
+         template_name: 'application')
   end
 
   def protect_against_forgery?
